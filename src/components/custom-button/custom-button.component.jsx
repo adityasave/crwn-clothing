3235249c6dabcd94f742474ad87da1da8ab1759b
adityasave <31876439+adityasave@users.noreply.github.com>
2020-05-17 21:24:20 +0530

    added reducer for adding items to cart and increasing ids on adding same item multiple times

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -5,11 +5,16 @@ import "./custom-button.style.scss";
 export default function CustomButton({
   children,
   isGoogleSignin,
+  isInverted,
   ...otherProps
 }) {
   return (
     <button
-      className={`${isGoogleSignin && "google-sign-in"} custom-button`}
+      className={`
+        ${isInverted && "inverted"} 
+        ${isGoogleSignin && "google-sign-in"}
+        custom-button
+      `}
       {...otherProps}
     >
       {children}
